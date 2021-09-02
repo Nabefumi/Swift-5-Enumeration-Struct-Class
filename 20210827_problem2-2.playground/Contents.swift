db895b481e1d9e23fdb822025dd488bea69f56a5
Takafumi Watanabe <36560121+Nabefumi@users.noreply.github.com>
2021-09-01 19:08:58 -0700

    Update Contents.swift

--- 20210827_problem2-2.playground/Contents.swift
+++ 20210827_problem2-2.playground/Contents.swift
@@ -62,3 +62,17 @@ let point3 = Point(x: 10, y: 20)
 let pointArrayList: [Point] = [point1, point2, point3]
 let averagePoint: Point = Point.vectorAverage(pointArrayList)
 averagePoint.printPoint(pt: averagePoint)
+
+// - Let’s do some math:
+// o Define a structure called point
+// o Point has two integer variables called: x and y
+// o Create a method called reset which set the x and y to 0
+// o Define an instance method called printPoint, which takes a point and print its
+// coordination in this format: the point is at coordinate [x,y] where x and y are the x
+// and y coordination of the input point.
+// o Define a static method called average which takes an array of points as well as a
+// character. (so the method has 2 inputs). If the input charcter is ‘x’ then the
+// function calculates the average of x coordination of all points in the array and
+// returns it. Similarly if the input character is ‘y’ then it calculates the average of y
+// coordination of the input points. If the character is not ‘x’ or ‘y’ then the function
+// returns nil.
