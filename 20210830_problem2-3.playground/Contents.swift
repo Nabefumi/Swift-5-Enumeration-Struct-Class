2a3c2acfa238b7e1d95e014e91971e709b016518
Takafumi Watanabe <36560121+Nabefumi@users.noreply.github.com>
2021-09-01 19:09:54 -0700

    Update Contents.swift

--- 20210830_problem2-3.playground/Contents.swift
+++ 20210830_problem2-3.playground/Contents.swift
@@ -34,5 +34,16 @@ print(calculate(opt: Calculator.subtraction, num1:10, num2:2))
 print(calculate(opt: Calculator.multiplication, num1:10, num2:2))
 print(calculate(opt: Calculator.division, num1:10, num2:5))
 
-
-
+// - Define an enum called Calculator with 4 cases:
+// o Sum, Subtraction, Multiplication and Division
+// o The cases have an associated value which is of type (Int, Int)-> Int
+// o Create a static method called calculate for the enum Calculator which takes three
+// input parameters:
+// § num1: An integer
+// § num2: An integer
+// § opt: A Calculator enum
+// o This function uses a switch statement based on the opt and each case return the
+// result of calling the function which is the associated value of the case. For
+// instance if the opt is Sum, the function will calculate the sum of the two input
+// integers (num1 and num2)
+// o Test: Write some test cases to test the enum and calling the calculate method.
